b3f8a46cd9c99ea9b58813ec18dd7c010c794650
Mehar <mehar.lall@gmail.com>
2025-06-15 17:46:49 +1200

    Setup OpenMeteo api and gets weather

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ function success(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
 
-  console.log(lat, lon)
+  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); // All weather info
+      const weather = data.current_weather;
+      const weatherType = weather.weathercode
+      console.log(`Temperature: ${weather.temperature}°C`);
+      console.log(`Latitude: ${lat}`)
+      console.log(`Longitude: ${lon}`)
+      console.log(`Weather Code: ${weatherType}`)
+    })
+    .catch(error => {
+      console.error('Error fetching weather:', error);
+    });
 }
 
 function error() {
