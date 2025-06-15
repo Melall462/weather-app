86116f3cf26946bd777cd973ea44d53b6faba911
Mehar <mehar.lall@gmail.com>
2025-06-15 18:20:17 +1200

    Retieving weather data and outputing the data to console

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-x = document.querySelector('.cords')
+import { weatherCodeMap } from "./weather.js";
+
+const x = document.querySelector('.cords')
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -22,14 +24,42 @@ function success(position) {
       console.log(`Latitude: ${lat}`)
       console.log(`Longitude: ${lon}`)
       console.log(`Weather Code: ${weatherType}`)
+      displayWeather(weatherType);
+      getCityFromCoords(lat, lon);
     })
     .catch(error => {
       console.error('Error fetching weather:', error);
     });
 }
 
-function error() {
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+  x.innerHTML = `Error: ${err.message}`;
   alert("Sorry, no position available.");
+}
+
+
+function displayWeather(code) {
+  const weatherCode = weatherCodeMap[code];
+
+  if (weatherCode) {
+    console.log(weatherCode.emoji);
+    console.log(weatherCode.description);
+  }
+}
+
+function getCityFromCoords(lat, lon) {
+  fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`)
+    .then(res => res.json())
+    .then(data => {
+      const country = data.address.country;
+      const village = data.address.village
+      const city = data.address.city;
+      console.log("City:", country + ", " + village + ', ' + city);
+      const parts = [country, village, city].filter(Boolean); // removes falsy values
+      document.querySelector(".cords").innerHTML += `<br>City: ${parts.join(", ") || "Unknown"}`;
+    })
+    .catch(err => console.error("Error fetching city:", err));
 }
 
 getLocation();
